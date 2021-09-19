e437ae4b9fb13c70c11ff4f7ce75852048fb448a
Manuel Entrena <manuel.entrena@gmail.com>
2021-09-19 23:04:39 +0200

    Added PageError

--- src/pages/PageError.js
+++ src/pages/PageError.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Footer } from "components";
+import { useHistory } from "react-router";
+import lottie from "lottie-web";
 
 export default function PageError() {
+  const history = useHistory();
+  const containerError = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: containerError.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../assets/errorPage.json"),
+    });
+  }, []);
+
+  const handleClick = () => {
+    history.push("/");
+  };
   return (
-    <div className="container">
-      <h1>PageError</h1>
-    </div>
+    <>
+      <div className="container">
+        <h1>TASK LIST</h1>
+        <p>No exite esta página, lo sentimos!</p>
+        <div ref={containerError}></div>
+        <button className="form__button" onClick={handleClick}>
+          IR AL INICIO
+        </button>
+      </div>
+      <Footer />
+    </>
   );
 }

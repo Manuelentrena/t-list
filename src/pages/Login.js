b0215f839ba1d3012328a096d319510c43e62b2c
Manuel Entrena <manuel.entrena@gmail.com>
2021-09-19 23:13:26 +0200

    Added animations inicial

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { FormLogin, Footer } from "components";
+import lottie from "lottie-web";
 
 export default function Login() {
+  const containerTask = useRef(null);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: containerTask.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../assets/animationTask.json"),
+    });
+  }, []);
   return (
     <>
       <div className="container">
         <h1>TASK LIST</h1>
         <FormLogin />
+        <div ref={containerTask}></div>
       </div>
       <Footer />
     </>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { FormRegister, Footer } from "components";
+import lottie from "lottie-web";
 
 export default function Register() {
+  const containerTask = useRef(null);
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: containerTask.current,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: require("../assets/animationTask.json"),
+    });
+  }, []);
   return (
     <>
       <div className="container">
         <h1>TASK LIST</h1>
         <FormRegister />
+        <div ref={containerTask}></div>
       </div>
       <Footer />
     </>
